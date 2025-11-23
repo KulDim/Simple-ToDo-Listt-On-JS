c3954964b56f739d64c985aeac391ee5d923097b
Вадим <61011733+KulDim@users.noreply.github.com>
2025-11-23 19:40:37 +0300

    fn edit and dell

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ const $add = document.querySelector("#add")
 const $modal = document.querySelector("#itemModal")
 
 const list = [
-    {id: 0, note: "hello", checked: false},
-    {id: 1, note: "hello 2", checked: true},
-    {id: 2, note: "hello 2", checked: true},
+    {id: 0, note: "hello", checked: false, edit: false},
+    {id: 1, note: "hello 2", checked: true, edit: false},
+    {id: 2, note: "hello 3", checked: true, edit: false},
 ]
 
-
-console.log($list);
 
 function renderList(){
     $list.innerHTML = ""
@@ -21,35 +19,52 @@ function renderList(){
                     <span class="checkbox" ${item.checked ? "style='text-decoration: line-through'" : ""}>${item.note}</span>
                 </label>
                 <div class="controler">
-                    <button>edit</button>
-                    <button>dell</button>
+                    <button id="edit">edit</button>
+                    <button id="dell">dell</button>
                 </div>
             </div>`
     })
 }
 
 function renderListById(id){
+    
     for(item of list){
         if(item.id === id){
             const itemToDo = document.querySelector(`[data-id="${id}"]`);
-            itemToDo.innerHTML = `                
-                <label for="item_${item.id}">
-                    <input class="checkbox" type="checkbox" id="item_${item.id}" name="item_${item.id}" value="true" ${item.checked ? "checked" : ""}>
-                    <span class="checkbox" ${item.checked ? "style='text-decoration: line-through'" : ""}>${item.note}</span>
-                </label>
-                <div class="controler">
-                    <button>edit</button>
-                    <button>dell</button>
-                </div>`
-            break
+            if(item.edit === true){
+
+                itemToDo.innerHTML = `                
+                    <label for="item_${item.id}">
+                        <input type="text" name="note" value="${item.note}" id="inputNote">
+                    </label>
+                    <div class="controler">
+                        <button id="save">save</button>
+                        <button id="dell">dell</button>
+                    </div>`
+            }else{
+
+                itemToDo.innerHTML = `                
+                    <label for="item_${item.id}">
+                        <input class="checkbox" type="checkbox" id="item_${item.id}" name="item_${item.id}" value="true" ${item.checked ? "checked" : ""}>
+                        <span class="checkbox" ${item.checked ? "style='text-decoration: line-through'" : ""}>${item.note}</span>
+                    </label>
+                    <div class="controler">
+                        <button id="edit">edit</button>
+                        <button id="dell">dell</button>
+                    </div>`
+            }
+
+            return
         }
     }
 }
 
-renderList()
 $list.addEventListener("click", (e) => {
     const itemElement = e.target.closest('.item');
     if (!itemElement) return;
+
+    if(e.target.id == "inputNote") return
+    
     
     const itemId = parseInt(itemElement.dataset.id);
     const item = list.find(i => i.id === itemId);
@@ -57,11 +72,30 @@ $list.addEventListener("click", (e) => {
     if (!item) return;
 
     const className = e.target.className
+    const id = e.target.id
 
+    
     if(className == "item" || className == "checkbox"){
         item.checked = !item.checked
     }
     
+    if(id == "dell"){
+        removeItemByIdMutable(list, itemId);
+        renderList()
+        return
+    }
+    
+    if(id == "edit"){
+        item.edit = true
+    }
+
+    if(id == "save"){
+        item.edit = false
+        item.note = itemElement.querySelector("#inputNote").value
+    }
+
+    
+
     renderListById(itemId)
 })
 
@@ -69,14 +103,28 @@ $add.addEventListener("click", (e) => {
     $modal.style.display = "block"
 })
 
+$modal.querySelector("[name='note']").addEventListener('keydown', (e) => {
+    const input = $modal.querySelector("[name='note']")
+
+    if (e.key === 'Enter') {
+        addTodoInput(input)
+    }
+})
+
 $modal.addEventListener("click", (e) => {
+    const input = $modal.querySelector("[name='note']")
 
     if(e.target.id == "itemModal" || e.target.id == "cancel"){
         $modal.style.display = "none"
     }
 
     if(e.target.id == "apply"){
-        const input = $modal.querySelector("[name='note']")
+        addTodoInput(input)
+    }
+})
+
+
+function addTodoInput(input){
         if(input.value == 0){
             input.placeholder = "Enter the value...";
             return
@@ -90,5 +138,14 @@ $modal.addEventListener("click", (e) => {
         input.placeholder = "Input your note...";
         input.value = ""
         renderList()
+}
+
+function removeItemByIdMutable(arr, id) {
+    const index = arr.findIndex(item => item.id === id);
+    if (index !== -1) {
+        arr.splice(index, 1);
     }
-})
+    return arr;
+}
+
+renderList()
