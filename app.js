6d096d1ba887205e9dd841c2052dc2ad6b19784f
Вадим <61011733+KulDim@users.noreply.github.com>
2025-11-23 17:55:47 +0300

    fn add

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 const $list = document.querySelector("#list")
+const $add = document.querySelector("#add")
+const $modal = document.querySelector("#itemModal")
 
 const list = [
     {id: 0, note: "hello", checked: false},
     {id: 1, note: "hello 2", checked: true},
     {id: 2, note: "hello 2", checked: true},
-    {id: 3, note: "hello 2", checked: true},
-    {id: 4, note: "hello 2", checked: true},
-    {id: 5, note: "hello 2", checked: true},
-    {id: 6, note: "hello 2", checked: true},
-    {id: 7, note: "hello 2", checked: true},
-    {id: 8, note: "hello 2", checked: true},
-    {id: 9, note: "hello 2", checked: true},
-    {id: 10, note: "hello 2", checked: true},
-    {id: 11, note: "hello 2", checked: true},
-    {id: 12, note: "hello 2", checked: true},
 ]
 
 
@@ -37,7 +29,6 @@ function renderList(){
 }
 
 function renderListById(id){
-
     for(item of list){
         if(item.id === id){
             const itemToDo = document.querySelector(`[data-id="${id}"]`);
@@ -55,8 +46,6 @@ function renderListById(id){
     }
 }
 
-
-
 renderList()
 $list.addEventListener("click", (e) => {
     const itemElement = e.target.closest('.item');
@@ -69,12 +58,37 @@ $list.addEventListener("click", (e) => {
 
     const className = e.target.className
 
-
     if(className == "item" || className == "checkbox"){
         item.checked = !item.checked
-
     }
     
+    renderListById(itemId)
+})
 
-     renderListById(itemId)
+$add.addEventListener("click", (e) => {
+    $modal.style.display = "block"
+})
+
+$modal.addEventListener("click", (e) => {
+
+    if(e.target.id == "itemModal" || e.target.id == "cancel"){
+        $modal.style.display = "none"
+    }
+
+    if(e.target.id == "apply"){
+        const input = $modal.querySelector("[name='note']")
+        if(input.value == 0){
+            input.placeholder = "Enter the value...";
+            return
+        }
+        list.push({
+            id: list.length,
+            note: input.value,
+            checked: false
+        })
+        $modal.style.display = "none"
+        input.placeholder = "Input your note...";
+        input.value = ""
+        renderList()
+    }
 })
